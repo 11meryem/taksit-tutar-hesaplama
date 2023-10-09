9f1b291d838c032f6869392ba86e6f1efc553c9e
11meryem <145452149+11meryem@users.noreply.github.com>
2023-10-09 16:41:51 +0300

    Add files via upload

---
+++ button.23/button.23/button.23/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace button._23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblfiyat = new System.Windows.Forms.Label();
+            this.lblTaksit = new System.Windows.Forms.Label();
+            this.lblTutar = new System.Windows.Forms.Label();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.rb0 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb6 = new System.Windows.Forms.RadioButton();
+            this.rb9 = new System.Windows.Forms.RadioButton();
+            this.rb12 = new System.Windows.Forms.RadioButton();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb12);
+            this.groupBox1.Controls.Add(this.rb9);
+            this.groupBox1.Controls.Add(this.rb6);
+            this.groupBox1.Controls.Add(this.rb2);
+            this.groupBox1.Controls.Add(this.rb0);
+            this.groupBox1.Location = new System.Drawing.Point(25, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(141, 210);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seçim Yap";
+            // 
+            // lblfiyat
+            // 
+            this.lblfiyat.AutoSize = true;
+            this.lblfiyat.Location = new System.Drawing.Point(6, 29);
+            this.lblfiyat.Name = "lblfiyat";
+            this.lblfiyat.Size = new System.Drawing.Size(29, 13);
+            this.lblfiyat.TabIndex = 1;
+            this.lblfiyat.Text = "Fiyat";
+            // 
+            // lblTaksit
+            // 
+            this.lblTaksit.AutoSize = true;
+            this.lblTaksit.Location = new System.Drawing.Point(6, 39);
+            this.lblTaksit.Name = "lblTaksit";
+            this.lblTaksit.Size = new System.Drawing.Size(66, 13);
+            this.lblTaksit.TabIndex = 2;
+            this.lblTaksit.Text = "Taksit Tutarı";
+            // 
+            // lblTutar
+            // 
+            this.lblTutar.AutoSize = true;
+            this.lblTutar.Location = new System.Drawing.Point(6, 67);
+            this.lblTutar.Name = "lblTutar";
+            this.lblTutar.Size = new System.Drawing.Size(70, 13);
+            this.lblTutar.TabIndex = 3;
+            this.lblTutar.Text = "Toplam Tutar";
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(54, 26);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 20);
+            this.txtFiyat.TabIndex = 4;
+            // 
+            // rb0
+            // 
+            this.rb0.AutoSize = true;
+            this.rb0.Location = new System.Drawing.Point(7, 29);
+            this.rb0.Name = "rb0";
+            this.rb0.Size = new System.Drawing.Size(74, 17);
+            this.rb0.TabIndex = 0;
+            this.rb0.TabStop = true;
+            this.rb0.Text = "Peşin (%0)";
+            this.rb0.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(6, 61);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(86, 17);
+            this.rb2.TabIndex = 1;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "2 Taksit (%2)";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb6
+            // 
+            this.rb6.AutoSize = true;
+            this.rb6.Location = new System.Drawing.Point(6, 99);
+            this.rb6.Name = "rb6";
+            this.rb6.Size = new System.Drawing.Size(86, 17);
+            this.rb6.TabIndex = 2;
+            this.rb6.TabStop = true;
+            this.rb6.Text = "6 Taksit (%4)";
+            this.rb6.UseVisualStyleBackColor = true;
+            // 
+            // rb9
+            // 
+            this.rb9.AutoSize = true;
+            this.rb9.Location = new System.Drawing.Point(6, 136);
+            this.rb9.Name = "rb9";
+            this.rb9.Size = new System.Drawing.Size(86, 17);
+            this.rb9.TabIndex = 3;
+            this.rb9.TabStop = true;
+            this.rb9.Text = "9 Taksit (%7)";
+            this.rb9.UseVisualStyleBackColor = true;
+            // 
+            // rb12
+            // 
+            this.rb12.AutoSize = true;
+            this.rb12.Location = new System.Drawing.Point(7, 172);
+            this.rb12.Name = "rb12";
+            this.rb12.Size = new System.Drawing.Size(98, 17);
+            this.rb12.TabIndex = 4;
+            this.rb12.TabStop = true;
+            this.rb12.Text = "12 Taksit (%10)";
+            this.rb12.UseVisualStyleBackColor = true;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(54, 69);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 45);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtFiyat);
+            this.groupBox2.Controls.Add(this.btnHesapla);
+            this.groupBox2.Controls.Add(this.lblfiyat);
+            this.groupBox2.Location = new System.Drawing.Point(182, 46);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(170, 131);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bilgi Girişi";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblTaksit);
+            this.groupBox3.Controls.Add(this.lblTutar);
+            this.groupBox3.Location = new System.Drawing.Point(191, 202);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(161, 113);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Çıkış";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 362);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb12;
+        private System.Windows.Forms.RadioButton rb9;
+        private System.Windows.Forms.RadioButton rb6;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb0;
+        private System.Windows.Forms.Label lblfiyat;
+        private System.Windows.Forms.Label lblTaksit;
+        private System.Windows.Forms.Label lblTutar;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ button.23/button.23/button.23/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace button._23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double fiyat = 0, toplamTutar = 0, taksitTutar = 0;
+
+            fiyat = Convert.ToDouble(txtFiyat.Text);
+
+            if (rb0.Checked)
+            {
+                toplamTutar = fiyat;
+                taksitTutar = toplamTutar / 1;
+            }
+            else if (rb2.Checked)
+            {
+                toplamTutar = fiyat * 1.02;
+                taksitTutar = toplamTutar / 2;
+            }
+            else if (rb6.Checked)
+            {
+                toplamTutar = fiyat * 1.04;
+                taksitTutar = toplamTutar / 6;
+            }
+            else if (rb9.Checked)
+            {
+                toplamTutar = fiyat * 1.07;
+                taksitTutar = toplamTutar / 9;
+            }
+            else if (rb12.Checked)
+            {
+                toplamTutar = fiyat * 1.1;
+                taksitTutar = toplamTutar / 12;
+            }
+
+            lblTutar.Text = "Toplam Tutar: " + toplamTutar;
+            lblTaksit.Text = "Taksit: " + taksitTutar;
+
+
+        }
+
+    }
+}
